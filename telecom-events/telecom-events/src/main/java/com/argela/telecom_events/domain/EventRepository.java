3f9b441be3496e31db66732fbb28a9286b6708c6
Yunus Can <73487916+dmlpYunus@users.noreply.github.com>
2025-11-16 22:45:57 +0300

    Delete telecom-events directory

--- telecom-events/telecom-events/src/main/java/com/argela/telecom_events/domain/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.argela.telecom_events.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
-}
